8f7892e703163ebe347b6482928f39ce338ef6a3
Vijipaddu <63455294+Vijipaddu@users.noreply.github.com>
2021-12-15 11:11:44 -0800

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,50 @@
+<?php 
+echo "Hello world";
+// session_start();
+// include "DB_Conn.php";
+
+// if(isset($_POST['uname']) && isset($_POST['password'])) {
+    
+//     function validate($data) {
+//         $data = trim($data);
+//         $data = stripslashes($data);
+//         $data = htmlspecialchars($data);
+//         return data;
+//     }
+// }
+
+// $uname = validate($_POST['uname']);
+// $pass = validate($_POST['password']);
+
+// if(empty($uname)) {
+//     header ("Location : index.php?error=User Name is required");
+//     exit();
+// }
+// else if(empty($pass)) {
+//     header ("Location : index.php?error=Passsword is required");
+//     exit();
+// }
+
+// $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+
+// $result = mysqli_query($conn, $sql);
+
+// if(mysqli_num_rows($result) === 1) {
+//     $row = mysqli_fetch_assoc($result);
+//     if($row['user_name'] === $uname && $row['password'] === $pass) {
+//         echo "Logged IN!";
+//         $_SESSION['user_name'] = $row['user_name'];
+//         $_SESSION['name'] = $row['name'];
+//         $_SESSION['id'] = $row['id'];
+//         header("Location: home.php");
+//         exit();
+//     }
+//     else{
+//         header("Location: index.php>error=Incorrect User Name or Password ")
+//     }
+// }
+// else {
+//     header("Location: index.php");
+//     exit();
+// }
+?>
